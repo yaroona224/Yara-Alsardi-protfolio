624575f03daf14bd47cfc3b21073e27feb7654e3
yaroona224 <162414446+yaroona224@users.noreply.github.com>
2024-03-18 07:39:42 +0000

    add projects to web application and interact dynamically

--- app.py
+++ app.py
@@ -1,22 +1,15 @@
-from flask import Flask,render_template
-app=Flask(__name__)
-projects = [
-{'id':1,
- 'project' : 'Web Scrapping',
-'skills' : 'urllib python, python requests, selenium'
-},
-{'id':2,
- 'project' : 'AI model for flare mointoring',
-'skills' : 'pthyon pandas, microsft azure, python, pychatgpt'},
-{'id':3,
- 'project' : 'Energy innovation',
-'skills' : 'python, flask, html, css, javascript, bootstrap'}
-]
+from flask import Flask, render_template
+from database import load_projects_from_db
 
-@app.route('/')
+app = Flask(__name__)
+
+@app.route("/")
 def hello_world():
-  return render_template('home.html', myskills = projects,
-                        myname = 'Yara')
-  
-if __name__=='__main__':
-  app.run(host="0.0.0.0",port=True)#run flask server
+  projects = load_projects_from_db()
+  return render_template('home.html',
+                         myskills=projects,
+                         myname='Yara Alsardi')
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=True)  #Run Flask server
--- database.py
+++ database.py
@@ -1,6 +1,21 @@
 from sqlalchemy import create_engine,text
-engine = create_engine("mysql+pymysql://admin:1234admin@sdatabase-1.cbuy2wsyk2rn.eu-north-1.rds.amazonaws.com/project?charset=utf8mb4")
+import os
+db_connection_string=os.environ['DB_CONNECTION_STRING']
+
+engine = create_engine(db_connection_string)
 
 with engine.connect() as conn:
-  result = conn.execute(text("select * from trial"))
+  result = conn.execute(text("select * from myprojects"))
   print(result.all())
+  from sqlalchemy import create_engine, text
+
+
+
+  def load_projects_from_db():
+     with engine.connect() as conn:
+        result = conn.execute(text("select * from myprojects"))
+        myprojects = []
+        for row in result.all():
+          myprojects.append(dict(row._mapping))
+        return myprojects
+    
