d40055be5a75dceccb172670b7b27e7b894512bc
yaroona224 <162414446+yaroona224@users.noreply.github.com>
2024-03-21 11:03:23 +0000

    receive message and save to db

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from database import load_projects_from_db
+from flask import Flask, render_template, request
+from database import load_projects_from_db,load_project_from_db,add_message_to_db
 
 app = Flask(__name__)
 
@@ -10,6 +10,19 @@ def hello_world():
                          myskills=projects,
                          myname='Yara Alsardi')
 
+@app.route("/project/<id>")
+def show_project(id):
+  project = load_project_from_db(id)
+  return render_template('projectdetails.html', project=project)
 
+@app.route("/contactme")
+def contactpage():
+  return render_template('contactme.html')
+
+@app.route("/message", methods = ['post'])
+def message():
+  data=request.form
+  add_message_to_db(data)
+  return render_template("messag_submitted.html", message=data)
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=True)  #Run Flask server
--- database.py
+++ database.py
@@ -1,21 +1,32 @@
-from sqlalchemy import create_engine,text
+from sqlalchemy import create_engine, insert, text
 import os
-db_connection_string=os.environ['DB_CONNECTION_STRING']
+
+db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(db_connection_string)
 
-with engine.connect() as conn:
-  result = conn.execute(text("select * from myprojects"))
-  print(result.all())
-  from sqlalchemy import create_engine, text
+
+def load_projects_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from myprojects"))
+    myprojects = []
+    for row in result.all():
+      myprojects.append(dict(row._mapping))
+    return myprojects
 
 
+def load_project_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(f"select * from myprojects where id ={id}"))
+  rows = result.all()
+  return dict(rows[0]._mapping)
 
-  def load_projects_from_db():
-     with engine.connect() as conn:
-        result = conn.execute(text("select * from myprojects"))
-        myprojects = []
-        for row in result.all():
-          myprojects.append(dict(row._mapping))
-        return myprojects
-    
+def add_message_to_db(data):
+  with engine.connect() as conn:
+    sql=text(
+  f"INSERT INTO messages (full_name,email,message) VALUES (\'{data['full_name']}\',\'{data['email']}\',\'{data['message']}\')"
+  )
+
+  
+    conn.execute(sql)
+    conn.commit()
